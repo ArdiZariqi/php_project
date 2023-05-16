835146dfeb89f5f178df8eb9aded54b25873d3a5
ArditGjyrevci <ardit.gjyrevci@student.uni-pr.edu>
2023-05-16 21:41:01 +0200

    c1

--- admin/course-add.php
+++ admin/course-add.php
@@ -72,12 +72,6 @@ if (
   } else {
     header("Location: ../login.php");
     exit;
-<<<<<<< HEAD
-  } else {
-	header("Location: ../login.php");
-	exit;
-} 
-=======
   }
 } else {
   header("Location: ../login.php");
@@ -85,4 +79,3 @@ if (
 }
 
 ?>
->>>>>>> 255f8bf988d3f20c7b4bc897e26f2a2e31db52e2
