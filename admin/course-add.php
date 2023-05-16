f81af8c4135bbed1e22e55026e82fbaaec801a09
agnesajashanicaa <122025593+agnesajashanicaa@users.noreply.github.com>
2023-05-16 21:26:12 +0200

    commit

--- admin/course-add.php
+++ admin/course-add.php
@@ -59,8 +59,7 @@ if (isset($_SESSION['admin_id']) &&
       <button type="submit" class="btn btn-primary">Create</button>
      </form>
      </div>
-     <?php } ?>
-
+    
      
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"></script>	
     <script>
