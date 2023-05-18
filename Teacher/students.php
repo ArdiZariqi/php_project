aa40008c1536f05c5fb53147d5cc79b3d7be6cda
ArijesaMuja <arijesa.muja@student.uni-pr.edu>
2023-05-18 03:53:04 -0800

    change5.1

--- Teacher/students.php
+++ Teacher/students.php
@@ -21,17 +21,24 @@ if (isset($_SESSION['teacher_id']) &&
     <title>Teachers - Students</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/footer.css">
     <link rel="icon" href="../logo.png">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <style> 
-  body{
-    background-color: #f5f5f5;
-  }
-      .container {
-        max-width: 600px;
-        margin: 0 auto;
-      }
+    body{
+      background-color: #f5f5f5;
+    }
+    .container {
+	  		max-width: 800px;
+	  		margin: 0 auto;
+	  		padding: 20px;
+	  	}
+
+    .container-main{
+      margin-left: 300px;
+      margin-bottom: 60px;
+    }
       
       .n-table {
         width: 100%;
@@ -78,7 +85,7 @@ if (isset($_SESSION['teacher_id']) &&
         include "inc/navbar.php";
         if ($classes != 0) {
      ?>
-     <div class="container mt-5">
+     <div class="container-main mt-5">
 
            <div class="table-responsive">
               <table class="table table-bordered mt-3 n-table">
@@ -134,6 +141,51 @@ if (isset($_SESSION['teacher_id']) &&
     </script>
 
 </body>
+<footer class="site-footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12 col-md-6">
+            <h6>About</h6>
+            <p class="text-justify"> <i>BIT </i> is an initiative  to help the upcoming programmers with the code. <i>BIT</i> focuses on providing the most efficient code or snippets as the code wants to be simple. We will help programmers build up concepts in different programming languages that include C, C++, Java, HTML, CSS, Bootstrap, JavaScript, PHP, Android, SQL and Algorithm.</p>
+          </div>
+          <div class="col-xs-6 col-md-3">
+            <h6>Categories</h6>
+            <ul class="footer-links">
+              <li>C</a></li>
+              <li>C++</a></li>
+              <li>HTML</a></li>
+              <li>Java</a></li>
+              <li>CSS</a></li>
+              <li>Bootstrap</a></li>
+            </ul>
+          </div>
+
+          <div class="col-xs-6 col-md-3">
+            <h6>Quick Links</h6>
+            <ul class="footer-links">
+              <li><a href="#home">Home</a></li>
+              <li><a href="#about">About Us</a></li>
+              <li><a href="#contact">Contact Us</a></li>
+            </ul>
+          </div>
+        </div>
+        <hr>
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8 col-sm-6 col-xs-12">
+          </div>
+          <div class="col-md-4 col-sm-6 col-xs-12">
+            <ul class="social-icons">
+              <li><a class="facebook" href="#"><i class="fa fa-facebook"></i></a></li>
+              <li><a class="twitter" href="#"><i class="fa fa-twitter"></i></a></li>
+              <li><a class="instagram" href="#"><i class="fa fa-instagram"></i></a></li>
+              <li><a class="linkedin" href="#"><i class="fa fa-linkedin"></i></a></li>   
+            </ul>
+          </div>
+        </div>
+      </div>
+</footer>
 </html>
 <?php 
 
