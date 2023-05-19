65311d8c52b35b3e1b9c7cac5e9ae451cdbb7e34
ArijesaMuja <arijesa.muja@student.uni-pr.edu>
2023-05-19 03:04:23 -0800

    change

--- admin/student-view.php
+++ admin/student-view.php
@@ -29,6 +29,57 @@ if (
         <link rel="icon" href="../Logo 1_a v5.png">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <style> 
+        body {
+          background-color: #f5f5f5;
+        }
+
+        .container-main {
+          display: flex;
+          align-items: center;
+          margin: 100px;
+        }
+
+        .container {
+          max-width: 800px;
+          margin: 0 auto;
+          padding: 20px;
+        }
+
+        .figure {
+          max-width: 50%;
+          height: auto;
+          border-radius: 4px;
+          overflow: hidden;
+          margin-left: 50px;
+        }
+
+        .figure img {
+          max-width: 100%;
+          height: auto;
+          box-shadow: 0 0 10px rgba(0, 0, 0, 1);
+        }
+
+        .text {
+          padding: 20px;
+          margin-left: 180px;
+          height: auto;
+          width: 600px;
+          background-color: #ffffff;
+          box-shadow: 0 0 10px rgba(0, 0, 0, 1);
+        }
+
+        h2 {
+          font-size: 24px;
+          color: #333333;
+          margin-bottom: 10px;
+        }
+
+        p {
+          font-size: 14px;
+          color: #666666;
+        }
+        </style>
       </head>
 
       <body>
@@ -36,12 +87,18 @@ if (
         include "inc/navbar.php";
         if ($student != 0) {
         ?>
-          <div class="container mt-5">
+          <div class="container-main mt-5">
+          <div class="figure">
             <div class="card" style="width: 22rem;">
               <img src="../img/student-<?= $student['gender'] ?>.png" class="card-img-top" alt="...">
               <div class="card-body">
                 <h5 class="card-title text-center">@<?= $student['username'] ?></h5>
               </div>
+            </div>
+          </div>
+
+          
+          <div class="text">
               <ul class="list-group list-group-flush">
                 <li class="list-group-item">First name: <?= $student['fname'] ?></li>
                 <li class="list-group-item">Last name: <?= $student['lname'] ?></li>
@@ -59,7 +116,7 @@ if (
                   echo $s['section'];
                   ?>
                 </li>
-                <br><br>
+                
                 <li class="list-group-item">Parent first name: <?= $student['parent_fname'] ?></li>
                 <li class="list-group-item">Parent last name: <?= $student['parent_lname'] ?></li>
                 <li class="list-group-item">Parent phone number: <?= $student['parent_phone_number'] ?></li>
@@ -69,6 +126,7 @@ if (
               </div>
             </div>
           </div>
+          
         <?php
         } else {
           header("Location: teacher.php");
